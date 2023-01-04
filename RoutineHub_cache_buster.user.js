867e47292fe9483179fee0a545580533e5aa4db3
Antonio Bueno <atnbueno@gmail.com>
2023-01-04 21:34:41 +0100

    Slower timestamp hashed w/ cookies
    Timestamp resolution changed to 30 seconds, and timestamp now hashed with the cookies using SHA-256

--- RoutineHub_cache_buster.user.js
+++ RoutineHub_cache_buster.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        RoutineHub cache buster
-// @version     1.1.0
+// @version     1.2.0
 // @license     MIT
 // @author      https://github.com/atnbueno
 // @description A simple tweak to the internal links of routinehub.co to work around its caching issues
@@ -11,9 +11,24 @@
 // ==/UserScript==
 
 // v1.1.0: jQuery dependency removed, code simplified, and metadata added
+// v1.2.0: Timestamp resolution changed to 30 seconds, and timestamp now hashed with the cookies using SHA-256
 
-const timestamp = Math.floor(Date.now() / 1000);
-document.addEventListener('DOMContentLoaded', () => document
-  .querySelectorAll('a:not([href^="http"]):not([href*="?"])')
-  .forEach(link => link.setAttribute('href',
-    `${link.getAttribute('href')}?${timestamp}`)));
+async function sha256(message) {
+  const crypto = window.crypto.subtle;
+  const encoder = new TextEncoder();
+  const data = encoder.encode(message);
+  try {
+    const hash = await crypto.digest('SHA-256', data);
+    return btoa(String.fromCharCode(...new Uint8Array(hash)));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+(async () => {
+  const cacheBuster = await sha256(Math.floor(Date.now() / 30000) + document.cookie);
+  document.addEventListener('DOMContentLoaded', () => document
+    .querySelectorAll('a:not([href^="http"]):not([href*="?"])')
+    .forEach(link => link.setAttribute('href',
+      `${link.getAttribute('href')}?${cacheBuster}`)));
+})();
