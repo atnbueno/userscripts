cd419393957deba2a9b7c37a0c90e8fe9eff27fb
Antonio Bueno <atnbueno@gmail.com>
2024-02-18 20:11:23 +0100

    Adjusts homepage carousel size
    Adds a GM_addStyle polyfill for the "Userscripts" Safari extension. With it the size of the homepage carousel can be adjusted.

--- routinehub-tweaks.user.js
+++ routinehub-tweaks.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        RoutineHub tweaks
-// @version     2.0
+// @version     2.1
 // @license     MIT
 // @author      https://github.com/atnbueno
 // @description Experiments in improving the experience of using routinehub.co
@@ -8,10 +8,27 @@
 // @namespace   https://github.com/atnbueno/userscripts
 // @supportURL  https://github.com/atnbueno/userscripts/issues
 // @match       https://routinehub.co/*
+// @grant       GM_addStyle
 // ==/UserScript==
 
 (function () {
   "use strict";
+
+  if (typeof GM_addStyle == 'undefined') {
+    var GM_addStyle = (CSS) => {
+      'use strict';
+      let head = document.getElementsByTagName('head')[0];
+      if (head) {
+        let style = document.createElement('style');
+        style.setAttribute('type', 'text/css');
+        style.textContent = CSS;
+        head.appendChild(style);
+        return style;
+      }
+      return null;
+    };
+  }
+
   document.addEventListener("DOMContentLoaded", function(event) {
 
     // Adds retina version of images if they are available
@@ -19,6 +36,14 @@
       image.onerror = () => image.removeAttribute('srcset');
       image.setAttribute('srcset', image.src.replace(/^(.+?)\.(gif|jpe?g|png|webp).*$/g, "$1.$2, $1_2x.$2 2x"));
     });
+
+    // Adjusts the homepage carousel size
+    GM_addStyle(''+`
+      #carousel, .slick-slide {
+        margin: 0 auto;
+        max-width: 660px;
+      }
+    `);
 
   });
 })();
